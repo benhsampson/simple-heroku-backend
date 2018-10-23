de0a1713a8dfe7c792eddc80951ab5cfecb71d4a
Benjamin Sampson <bensampsondev@gmail.com>
2018-10-23 21:21:28 +1100

    Add Procfile

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hi' });
 });
